b2270a62da97e06760cf51d6fdacec5d21e3a112
gd <gentdupont@gmail.com>
2025-04-29 10:17:28 -0400

    transactions loading

---
+++ SolfunmemeLean/secret.md
@@ -0,0 +1,4 @@
+
+
+
+git remote set-url origin https://h4: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@huggingface.co/datasets/introspector/solfunmeme/ 
--- test.sh
+++ test.sh
@@ -1,2 +1,4 @@
 #curl -X POST -H "Content-Type: application/json" --data @test.json     "https://api.mainnet-beta.solana.com"
+SOLANA_URL="https://mainnet.helius-rpc.com/?api-key=57005be3-3fda-4438-ab6e-2107e578cce2"
+#$env:SOLANA_URL="https://mainnet.helius-rpc.com/?api-key=57005be3-3fda-4438-ab6e-2107e578cce2"
 C:\Users\gentd\.elan\toolchains\leanprover--lean4---v4.18.0\bin\lean.exe --run .\SolfunmemeLean\getSolfunmeme.lean
