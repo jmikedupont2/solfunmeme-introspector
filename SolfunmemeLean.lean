03811be424532297f38db31342333dd82b3f1047
gd <gentdupont@gmail.com>
2025-05-02 19:21:02 -0400

    wip

--- SolfunmemeLean.lean
+++ SolfunmemeLean.lean
@@ -1,3 +1,4 @@
 -- This module serves as the root of the `SolfunmemeLean` library.
 -- Import modules here that should be built as part of the library.
 import SolfunmemeLean.Basic
+import SolfunmemeLean.Common
